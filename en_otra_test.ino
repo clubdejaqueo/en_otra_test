6527a6a48ebaa9c9012ad6d16bad3f4aba8b2b08
Alejandro J. Cura <alecu@protocultura.net>
2015-07-23 17:43:39 -0300

    Una tabla programable de acciones para el motor

--- en_otra_test.ino
+++ en_otra_test.ino
@@ -1,16 +1,30 @@
-int tiron = 250;
-int afloje = 2600;
-int frena = 4000;
-int velMin = 60;
-int velMax = 255;
+const int tiron = 250;
+const int afloje = 2600;
+const int tiempo_variable = -1;
+
+const int velMin = -60;
+const int velMax = 255;
+const int velZero = 0;
 
 #define array_size(foo) (sizeof(foo)/sizeof(foo[0]))
 
+const int programas[] = {
+    velMax, tiron,
+    velMin, afloje,
+    velZero, tiempo_variable,
+};
+
+const byte len_programas = array_size(programas);
+
 class Motor {
     byte pin_pwm, pin_A, pin_B;
+    int delay_min, delay_max;
+    unsigned long tiempo_proximo_paso;
+    byte contador_programa;
   public:
-    Motor(byte pin_pwm, byte pin_A, byte pin_B) :
-      pin_pwm(pin_pwm), pin_A(pin_A), pin_B(pin_B) {
+    Motor(byte pin_pwm, byte pin_A, byte pin_B, int delay_min, int delay_max) :
+      pin_pwm(pin_pwm), pin_A(pin_A), pin_B(pin_B), delay_min(delay_min),
+      delay_max(delay_max), tiempo_proximo_paso(millis()), contador_programa(0) {
     }
 
     void setup() {
@@ -35,11 +49,44 @@ class Motor {
       digitalWrite(pin_A, LOW);
       digitalWrite(pin_B, LOW);
     }
+    
+    void cambiar_velocidad(int vel) {
+      if (vel == velZero) {
+        frenar();
+      }
+      if (vel > 0) {
+        avanzar(vel);
+      }
+      if (vel < 0) {
+        retroceder(abs(vel));
+      }
+    }
+    
+    int calcular_tiempo_variable() {
+      return random(delay_min, delay_max);
+    }
+    
+    void loop() {
+      if ((tiempo_proximo_paso-millis()) < 0) {
+        int velocidad = programas[contador_programa++];
+        cambiar_velocidad(velocidad);
+
+        int espera = programas[contador_programa++];
+        if (espera == tiempo_variable) {
+          espera = calcular_tiempo_variable();
+        }
+        tiempo_proximo_paso += espera;
+        
+        if (contador_programa > len_programas) {
+          contador_programa = 0;
+        }
+      }
+    }
 };
 
 Motor motores[] = {
-  {5, 6, 7},
-  {8, 9, 10},
+  {5, 6, 7, 500, 4000},
+  {8, 9, 10, 500, 4000},
 };
 
 const byte cantidad_motores = array_size(motores);
@@ -50,38 +97,8 @@ void setup() {
   }
 }
 
-
 void loop() {
-  Motor& m = motores[0];
-  
-  m.avanzar(velMax);
-  delay (tiron);
-
-  m.retroceder(velMin);
-  delay (afloje);
-
-  m.frenar();
-  delay (random(500, 4000));
-
-  m.avanzar(velMax);
-  delay (tiron);
-
-  m.retroceder(velMin);
-  delay (afloje);
-
-  m.frenar();
-  delay (random(500, 4000));
-
-  m.avanzar(velMax);
-  delay (tiron);
-
-  m.retroceder(velMin);
-  delay (afloje);
-
-  m.frenar();
-  delay (random(500, 4000));
+  for (int n = 0; n < cantidad_motores; n++) {
+    motores[n].loop();
+  }
 }
-
-
-
-
