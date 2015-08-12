98cea2812fe574164f3aa1fad6bff4d09d32b179
Alejandro J. Cura <alecu@protocultura.net>
2015-08-11 21:42:49 -0300

    Programas independientes, Arreglado el bug que no hace nada

--- en_otra_test.ino
+++ en_otra_test.ino
@@ -1,29 +1,44 @@
-const int tiempo_tiron = 250;
-const int tiempo_afloje = 2600;
+const int tiempo_tiron = 3500;
+const int tiempo_afloje = 3900;
 const int tiempo_variable = -1;
 
-const int velMin = -60;
-const int velMax = 255;
+const int velMin = -200;
+const int velMax = 210;
 const int velZero = 0;
+const int FIN_PROGRAMA = 9999;
 
-const int programas[] = {
+const int programa1[] = {
   velMax, tiempo_tiron,
   velMin, tiempo_afloje,
   velZero, tiempo_variable,
+  FIN_PROGRAMA
+};
+
+const int programa2[] = {
+  velMax, tiempo_tiron,
+  velMin, tiempo_afloje,
+  velZero, tiempo_variable,
+  FIN_PROGRAMA
 };
 
 #define array_size(foo) (sizeof(foo)/sizeof(foo[0]))
-const byte len_programas = array_size(programas);
+const byte len_programa1 = array_size(programa1);
+
 
 class Motor {
     byte pin_pwm, pin_A, pin_B;
     int delay_min, delay_max;
-    unsigned long tiempo_proximo_paso;
+    unsigned long tiempo_ultimo_paso;
+    int espera;
     byte contador_programa;
+    const int* programa;
+    int len_programa;
   public:
-    Motor(byte pin_pwm, byte pin_A, byte pin_B, int delay_min, int delay_max) :
+    Motor(byte pin_pwm, byte pin_A, byte pin_B, int delay_min, int delay_max,
+          const int* programa) :
       pin_pwm(pin_pwm), pin_A(pin_A), pin_B(pin_B), delay_min(delay_min),
-      delay_max(delay_max), tiempo_proximo_paso(millis()), contador_programa(0) {
+      delay_max(delay_max), programa(programa),
+      tiempo_ultimo_paso(0), contador_programa(0) {
     }
 
     void setup() {
@@ -33,23 +48,27 @@ class Motor {
     }
 
     void avanzar(int velocidad) {
+      Serial.println(" Avanzando");
       analogWrite(pin_pwm, velocidad);
       digitalWrite(pin_A, HIGH);
       digitalWrite(pin_B, LOW);
     }
 
     void retroceder(int velocidad) {
+      Serial.println(" Marcha Atras");
       analogWrite(pin_pwm, velocidad);
       digitalWrite(pin_A, LOW);
       digitalWrite(pin_B, HIGH);
     }
 
     void frenar() {
+      Serial.println(" Frenar");
       digitalWrite(pin_A, LOW);
       digitalWrite(pin_B, LOW);
     }
 
     void cambiar_velocidad(int vel) {
+      Serial.print(pin_pwm);
       if (vel == velZero) {
         frenar();
       }
@@ -66,39 +85,54 @@ class Motor {
     }
 
     void loop() {
-      if ((tiempo_proximo_paso - millis()) <= 0) {
-        int velocidad = programas[contador_programa++];
+      if ((millis() - tiempo_ultimo_paso) > espera) {
+        int velocidad = programa[contador_programa++];
         cambiar_velocidad(velocidad);
 
-        int espera = programas[contador_programa++];
+        espera = programa[contador_programa++];
         if (espera == tiempo_variable) {
           espera = calcular_tiempo_variable();
         }
-        tiempo_proximo_paso += espera;
+        Serial.print("Esperando...");
+        Serial.println(espera);
+        tiempo_ultimo_paso = millis();
 
-        if (contador_programa > len_programas) {
+        if (programa[contador_programa] == FIN_PROGRAMA) {
           contador_programa = 0;
+          return;
         }
       }
     }
 };
 
+
 Motor motores[] = {
   // pin_pwm, pin_A, pin_B, delay_min, delay_max
-  { 5, 6, 7, 500, 4000 },
-  { 8, 9, 10, 500, 4000 },
+  { 5, 6, 7, 500, 4000, programa1 },
+  { 8, 9, 10, 500, 4000, programa2 },
 };
 
 const byte cantidad_motores = array_size(motores);
 
 void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  Serial.print("Cantidad de motores: ");
+  Serial.println(cantidad_motores);
+
   for (int n = 0; n < cantidad_motores; n++) {
     motores[n].setup();
   }
+
 }
 
 void loop() {
+
   for (int n = 0; n < cantidad_motores; n++) {
     motores[n].loop();
   }
+
 }
